b930384a5d8bbc4c72c37d16b604beb7ca88a508
John Leavell <johnleavell@gmail.com>
2023-03-20 19:02:38 +0000

    started conditionals

--- conditionals_rps.rb
+++ conditionals_rps.rb
@@ -7,3 +7,22 @@
 # If you need a refresher on the rules of Rock, Paper, Scissors: https://en.wikipedia.org/wiki/Rock%E2%80%93paper%E2%80%93scissors
 
 p "Please choose rock, paper, or scissors:"
+
+player_input = gets.chomp.downcase
+
+player_input = "scissors"
+
+
+case player_input
+when "rock"
+p "You played rock!"
+p "You lost!"
+when "paper"
+p "You played paper!"
+p "You won!"
+when "scissors"
+p "You played scissors!"
+p "You tied!"
+else 
+p "Please select: rock, paper, or scissors:"
+end
